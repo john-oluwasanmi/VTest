6ee092b0b9c3fcfd1ca740d389adfe028db2a80c
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-21 21:27:27 +0100

    add interface

---
+++ V.Test.Web.App/Entities/Interface/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace V.Test.Web.App.Entities.Interface
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+        DateTime CreatedOn { get; set; }
+        DateTime? ModifiedOn { get; set; }
+        string CreatedFrom { get; set; }
+        bool? IsDeleted { get; set; }
+    }
+}
