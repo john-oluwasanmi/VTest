f6c77dfd2cafbe8d7f8dc19058f67a11a95c9144
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-21 22:20:22 +0100

    initial migration

--- V.Test.Web.App/Repository/VTestContext.cs
+++ V.Test.Web.App/Repository/VTestContext.cs
@@ -19,13 +19,83 @@ namespace V.Test.Web.App.Repository
         public virtual DbSet<Address> Address { get; set; }
         public virtual DbSet<Employee> Employee { get; set; }
         public virtual DbSet<Organisation> Organisation { get; set; }
-
+         
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Address>().ToTable("Address");
             modelBuilder.Entity<Employee>().ToTable("Employee");
             modelBuilder.Entity<Organisation>().ToTable("Organisation");
+
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.Property(e => e.AddressLine1)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AddressLine2)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AddressLine3)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AddressLine4)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Postcode)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Town)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Organisation)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.OrganisationId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Employee_Organisation");
+            });
+
+            modelBuilder.Entity<Organisation>(entity =>
+            {
+                entity.Property(e => e.OrganisationName)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.OrganisationNumber)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.Organisation)
+                    .HasForeignKey(d => d.AddressId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Organisation_Address");
+            });
         }
 
     }
