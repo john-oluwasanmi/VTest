ac7aee425710ffb0fa507099bef72c920ec5fe15
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-21 21:37:31 +0100

    add container and start up

---
+++ V.Test.Web.App/Core/Container.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using V.Test.Web.App.BusinessService;
+using V.Test.Web.App.BusinessService.Interface;
+using V.Test.Web.App.Repository;
+using V.Test.Web.App.Repository.Interface;
+
+namespace V.Test.Web.App.Core
+{
+    public static class Container
+    {
+        public static void AddService(IServiceCollection services)
+        {
+            services.AddScoped<IAddressBusinessService, AddressBusinessService>();
+            services.AddScoped<IEmployeeBusinessService, EmployeeBusinessService>();
+            services.AddScoped<IOrganisationBusinessService, OrganisationBusinessService>();
+        }
+
+        public static void AddRepository(IServiceCollection services)
+        {
+            services.AddScoped<IOrganisationRepository, OrganisationRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<AddressRepository, AddressRepository>();
+        }
+    }
+}
--- V.Test.Web.App/Startup.cs
+++ V.Test.Web.App/Startup.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using V.Test.Web.App.Core;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 namespace V.Test.Web.App
 {
     public class Startup
@@ -32,7 +32,37 @@ namespace V.Test.Web.App
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                 .AddJsonOptions(options =>
+                 {
+                     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                 });
+
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new VProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddAntiforgery();
+            services.AddHttpClient();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            services.AddScoped<IUrlHelper>(x =>
+            {
+                var actionContext = x.GetRequiredService<IActionContextAccessor>().ActionContext;
+                var factory = x.GetRequiredService<IUrlHelperFactory>();
+                return factory.GetUrlHelper(actionContext);
+            });
+
+            Container.AddService(services);
+            Container.AddRepository(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +78,19 @@ namespace V.Test.Web.App
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (ctx, next) =>
+            {
+                await next();
+
+                if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
+                {
+                    string originalPath = ctx.Request.Path.Value;
+                    ctx.Items["originalPath"] = originalPath;
+                    ctx.Request.Path = "/home/index";
+                    await next();
+                }
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
