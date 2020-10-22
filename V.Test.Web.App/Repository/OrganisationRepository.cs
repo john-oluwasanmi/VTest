ca2d3b05b8265f7cf54d193c8aa4909c5deb6d41
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-22 11:00:20 +0100

    override list() and get() of repos

--- V.Test.Web.App/Repository/OrganisationRepository.cs
+++ V.Test.Web.App/Repository/OrganisationRepository.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Threading.Tasks;
 using V.Test.Web.App.Entities;
 using V.Test.Web.App.Repository.Interface;
 
@@ -11,6 +17,66 @@ namespace V.Test.Web.App.Repository
         public OrganisationRepository(IConfiguration configuration,  ILogger<Organisation> logger)
               : base(configuration, logger)
         {
+        }
+        public override async Task<Organisation> GetAsync(int id)
+        {
+            try
+            {
+                using (var context = new VTestsContext(OptionsBuilder.Options))
+                {
+                    var entity = await context.Set<Organisation>()
+                                             .Where(x => x.Id == id && (x.IsDeleted == null || x.IsDeleted == false))
+                                             .Include(r => r.Address)
+                                            .SingleOrDefaultAsync();
+
+                    var typeName = Entity?.GetType()?.Name;
+                    VLogger.LogInformation($" Successfully retrieved {typeName} with the Id: '{entity?.Id} ");
+
+                    return entity;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError(ex, null, id);
+
+                throw ex;
+            }
+        }
+
+        public override async Task<List<Organisation>> ListAsync(int pageNumber)
+        {
+            try
+            {
+                CurrentPageNumber = pageNumber;
+
+                using (var context = new VTestsContext(OptionsBuilder.Options))
+                {
+                    List<Organisation> result = await context.Set<Organisation>().AsNoTracking()
+                                                .Where(e => e.IsDeleted == false || e.IsDeleted == null)
+                                                .OrderBy(r => r.Id)
+                                                .Skip(SkippedDbRecordSize)
+                                                .Take(MaxPageSize)
+                                                .ToListAsync();
+
+                    var typeName = Entity?.GetType()?.Name;
+                    VLogger.LogInformation($" Successfully retrieved {typeName}'s List with page number {pageNumber} ");
+                    CurrentPageNumber = 0;
+
+                    return result;
+                }
+            }
+            catch (SqlNullValueException s)
+            {
+                LogError(s, null, pageNumber);
+
+                throw s;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex, null, pageNumber);
+
+                throw ex;
+            }
         }
     }
 }
