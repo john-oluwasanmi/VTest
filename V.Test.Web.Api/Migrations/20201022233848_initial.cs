9d291e19127884de8ea553af3c8d4713eeb0ed81
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-23 00:39:05 +0100

    add migration

---
+++ V.Test.Web.Api/Migrations/20201022233848_initial.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace V.Test.Web.Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    AddressLine1 = table.Column<string>(unicode: false, maxLength: 150, nullable: false),
+                    AddressLine2 = table.Column<string>(unicode: false, maxLength: 150, nullable: true),
+                    AddressLine3 = table.Column<string>(unicode: false, maxLength: 150, nullable: true),
+                    AddressLine4 = table.Column<string>(unicode: false, maxLength: 150, nullable: true),
+                    Town = table.Column<string>(unicode: false, maxLength: 150, nullable: false),
+                    Postcode = table.Column<string>(unicode: false, maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organisation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    AddressId = table.Column<int>(nullable: true),
+                    OrganisationName = table.Column<string>(unicode: false, maxLength: 150, nullable: false),
+                    OrganisationNumber = table.Column<string>(unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organisation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Organisation_Address",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    OrganisationId = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(unicode: false, maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(unicode: false, maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Organisation",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organisation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_OrganisationId",
+                table: "Employee",
+                column: "OrganisationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Organisation_AddressId",
+                table: "Organisation",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Organisation");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
