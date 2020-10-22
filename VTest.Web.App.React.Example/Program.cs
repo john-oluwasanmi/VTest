018a86a729150a2fabd16a948fbe9a04d4552d51
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-23 00:38:32 +0100

    undo

--- VTest.Web.App.React.Example/Program.cs
+++ VTest.Web.App.React.Example/Program.cs
@@ -6,45 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using V.Test.Web.Api.Repository;
 
 namespace VTest.Web.App.React.Example
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
-
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<VTestsContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-
-
-         
-
-       
     }
 }
--- VTest.Web.App.React.Example/Startup.cs
+++ VTest.Web.App.React.Example/Startup.cs
@@ -1,24 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using AutoMapper;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using V.Test.Web.App.Core;
-using V.Test.Web.Api.Repository;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Routing;
-using V.Test.Web.App.Filter;
+
 namespace VTest.Web.App.React.Example
 {
     public class Startup
     {
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,77 +20,16 @@ namespace VTest.Web.App.React.Example
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<VTestsContext>(options =>
-                          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc(options =>
-            {
-                options.Filters.Add(new ActionFilterAttribute { });
-                options.Filters.Add(new ExceptionHandlerFilterAttribute { });
-            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                 .AddJsonOptions(options =>
-                 {
-                     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                 });
-
-
-            var mappingConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new VProfile());
-            });
-
-            IMapper mapper = mappingConfig.CreateMapper();
-            services.AddSingleton(mapper);
-
-            services.AddAntiforgery();
-
-            services.AddScoped<ActionFilterAttribute>();
-            services.AddScoped<ExceptionHandlerFilterAttribute>();
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
-
-            services.AddScoped<IUrlHelper>(x =>
-            {
-                var actionContext = x.GetRequiredService<IActionContextAccessor>().ActionContext;
-                var factory = x.GetRequiredService<IUrlHelperFactory>();
-                return factory.GetUrlHelper(actionContext);
-            });
-
+            // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
-
-
-            Container.AddService(services);
-            Container.AddRepository(services);
         }
 
-
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //    services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-        //    // In production, the React files will be served from this directory
-        //    services.AddSpaStaticFiles(configuration =>
-        //    {
-        //        configuration.RootPath = "ClientApp/build";
-        //    });
-        //}
-
-
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -134,7 +64,5 @@ namespace VTest.Web.App.React.Example
                 }
             });
         }
-
-
     }
 }
