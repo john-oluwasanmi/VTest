746f49b9a513c48aa07c6602b655b667338a6307
OLUWASANMI JOHN OLADIMEJI <kayasiah@yahoo.com>
2020-10-22 18:08:16 +0100

    add migreation to make address nullable on organisation

--- V.Test.Web.App/Migrations/20201022165636_addressNullable.Designer.cs
+++ V.Test.Web.App/Migrations/20201022165636_addressNullable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using V.Test.Web.App.Repository;
 
 namespace V.Test.Web.App.Migrations
 {
     [DbContext(typeof(VTestsContext))]
-    partial class VTestsContextModelSnapshot : ModelSnapshot
+    [Migration("20201022165636_addressNullable")]
+    partial class addressNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ V.Test.Web.App/Migrations/20201022165636_addressNullable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace V.Test.Web.App.Migrations
+{
+    public partial class addressNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Organisation",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Organisation",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
